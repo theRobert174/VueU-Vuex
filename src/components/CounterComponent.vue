477312fcaaef60a027454f4337d17b929d7fded5
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-01-08 14:09:00 -0600

    Mutations

--- src/components/CounterComponent.vue
+++ src/components/CounterComponent.vue
@@ -3,6 +3,10 @@
     <h2>Direct access: {{ $store.state.count }}</h2>
     <h2>Computed: {{ countComputed }}</h2>
 
+    <button @click="increment">+1</button>
+    <button @click="incrementBy">+5</button>
+    <button>Random</button>
+
     <h1>mapSate</h1>
     <h2>mapState: {{ count }}</h2>
     <h2>lastMutation: {{ lastMutation }}</h2>
@@ -17,6 +21,14 @@ export default {
         },
 
         ...mapState(['count', 'lastMutation'])
+    },
+    methods: {
+        increment(){
+            this.$store.commit('increment')
+        },
+        incrementBy(){
+            this.$store.commit('incrementBy',5)
+        }
     }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,5 +5,15 @@ export default createStore({
     state: {
         count: 1,
         lastMutation: 'none'
+    },
+    mutations: {//methods
+        increment(state){
+            state.count++
+            state.lastMutation = 'increment'
+        },
+        incrementBy(state, val){
+            state.count+=val
+            state.lastMutation = 'incrementBy'
+        }
     }
 })

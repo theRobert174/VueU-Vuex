38a0a0125ad183d830f374064ec20b86baa6c97e
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-01-08 13:51:40 -0600

    Storage State

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Counter - Vuex</h1>
+    <h2>Direct access: {{ $store.state.count }}</h2>
+    <h2>Computed: {{ countComputed }}</h2>
+
+    <h1>mapSate</h1>
+    <h2>mapState: {{ count }}</h2>
+    <h2>lastMutation: {{ lastMutation }}</h2>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+    computed: {
+        countComputed(){
+            return this.$store.state.count
+        },
+
+        ...mapState(['count', 'lastMutation'])
+    }
+}
+</script>
+
+<style>
+
+</style>
